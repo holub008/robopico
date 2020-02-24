52cac845752f0b1305b8cc7847df4994230dba45
Karl Holub <holub008@umn.edu>
2020-02-24 17:01:21 -0600

    pico endpoint handles batch

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ PICO_TIAB_BOT = PICOSpanRobot()
 PICO_TEXT_BOT = PICORobot()
 
 
+def validate_list(x):
+    return type(x) is list
+
+
 @app.route('/pico', methods=['POST'])
 def get_pico():
     body = request.get_json()
@@ -17,20 +21,33 @@ def get_pico():
     if not body:
         return jsonify({
             "message": "Must supply application/json data type"
-        }), 500
+        }), 400
+
+    results = []
 
     if 'abstract' in body and 'title' in body:
-        result = PICO_TIAB_BOT.annotate(body['abstract'], body['title'])
+        if validate_list(body['abstract']) and validate_list(body['title']) \
+                and len(body['title']) == len(body['abstract']):
+            for title, abstract in zip(body["title"], body["abstract"]):
+                results.append(PICO_TIAB_BOT.annotate(abstract, title))
+        else:
+            return jsonify({
+                "message": "'abstract' and 'title' must be lists of equal length"
+            }), 400
     elif 'full_text' in body:
-        result = PICO_TEXT_BOT.annotate(body['full_text'])
+        if validate_list(body['full_text']):
+            for text in body['full_text']:
+                results.append(PICO_TEXT_BOT.annotate(body['full_text']))
+        else:
+            return jsonify({
+                "message": "'full_text' must be a list"
+            }), 400
     else:
         return jsonify({
             "message": "Must supply {'abstract', title'} or {'full_text'}"
-        }), 500
+        }), 400
 
-    return jsonify({
-        "todo": result
-    })
+    return jsonify(results)
 
 
 if __name__ == "__main__":
