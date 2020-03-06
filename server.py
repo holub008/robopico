b9a203c799099e770577a2fc9d38183ea06740cd
Karl Holub <holub008@umn.edu>
2020-03-06 10:57:42 -0600

    better request validations

--- server.py
+++ server.py
@@ -11,9 +11,25 @@ PICO_TIAB_BOT = PICOSpanRobot()
 PICO_TEXT_BOT = PICORobot()
 STUDY_TYPE_BOT = StudyTypeRobot()
 
+# this is a guess, based on typical abstract length, of what should run in the span of a typical web request
+MAX_REQUEST_SIZE = 500
 
-def validate_list(x):
-    return type(x) is list
+
+def validate_length(*argv):
+    if len(argv) == 0:
+        return False
+
+    all_lists = [type(x) is list for x in argv]
+
+    if not all_lists:
+        return False
+
+    if len(argv) == 1:
+        return len(argv[0]) <= MAX_REQUEST_SIZE
+    else:
+        n = len(argv[0])
+        all_equal_length = all(len(x) == n for x in argv)
+        return all_equal_length and n <= MAX_REQUEST_SIZE
 
 
 @app.route('/pico', methods=['POST'])
@@ -28,8 +44,7 @@ def get_pico():
     results = []
 
     if 'abstract' in body and 'title' in body:
-        if validate_list(body['abstract']) and validate_list(body['title']) \
-                and len(body['title']) == len(body['abstract']):
+        if validate_length(body['title'], body['abstract']):
             for title, abstract in zip(body["title"], body["abstract"]):
                 annotation = PICO_TIAB_BOT.annotate(abstract, title)
                 # TODO: it may be faster to batch this (internal to StudyTypeRobot)
@@ -37,15 +52,15 @@ def get_pico():
                 results.append(annotation)
         else:
             return jsonify({
-                "message": "'abstract' and 'title' must be lists of equal length"
+                "message": "'abstract' and 'title' must be lists of equal length, less than or equal to %d" % (MAX_REQUEST_SIZE,)
             }), 400
     elif 'full_text' in body:
-        if validate_list(body['full_text']):
+        if validate_length(body['full_text']):
             for text in body['full_text']:
                 results.append(PICO_TEXT_BOT.annotate(text))
         else:
             return jsonify({
-                "message": "'full_text' must be a list"
+                "message": "'full_text' must be a list with length less than or equal to %d" % (MAX_REQUEST_SIZE, )
             }), 400
     else:
         return jsonify({
